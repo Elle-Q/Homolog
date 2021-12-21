82eebac23d1c3af3e9a92fb00fdca67f958dbcda
elle <1127198682@qq.com>
2021-12-21 17:05:06 +0800

    add router page transition

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from "./app/store";
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
+import {CSSTransition, SwitchTransition} from "react-transition-group";
+import "./styles.css";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+            <SwitchTransition mode="out-in">
+            <CSSTransition key={"Goodbye, world!"}
+                           addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
+                           classNames='fade'>
+                <App/>
+            </CSSTransition>
+        </SwitchTransition>
+    </Provider>
+</React.StrictMode>,
+document.getElementById('root')
+)
+;
 
